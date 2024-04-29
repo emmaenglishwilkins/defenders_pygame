12a97a06285d58a70ef8e8c71320d71caed82df5
Emma Wilkins <emmawilkins@penguincodingschool.com>
2024-04-29 18:49:30 -0400

    enemy class completed

--- enemy.py
+++ enemy.py
@@ -1,7 +1,20 @@
 import pygame
-
+import random
 class Enemy:
+    def __init__(self, surface):
+        img = pygame.image.load("")
+        self.img = pygame.transform.scale(100,100)
+        self.rect = self.img.get_rect(center=(350,500))
+        self.surface = surface
+        self.speed = 1
     def draw(self):
-        pass
+        self.surface.blit(self.img, self.rect)
     def move(self, player, game):
-        pass
+        self.rect.y += self.speed
+        if self.rect.y >= 600:
+            self.rect.y = 0
+            self.rect.x = random.randint(0,800)
+        if self.rect.colliderect(player.rect):
+            game.lives -= 1
+            self.rect.y = 0
+            self.rect.x = random.randint(0,800)
