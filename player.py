e2a014736832ba8afab7fb644d459626cfc53d15
Emma Wilkins <emmawilkins@penguincodingschool.com>
2024-04-29 18:51:04 -0400

    player class completed

--- player.py
+++ player.py
@@ -1,7 +1,15 @@
 import pygame
-
 class Player:
+    def __init__(self, surface):
+        img = pygame.image.load("emma_defender.png") # add name of player png file here
+        self.img = pygame.transform.scale(img, (100, 100))
+        self.rect = self.img.get_rect(center=(350, 500))
+        self.surface = surface
+        self.xdir = 0
+        self.speed = 1
     def draw(self):
-        pass
+        self.surface.blit(self.img, self.rect)
     def move(self, xdir):
-        pass
+        self.rect.x += xdir * self.speed
+        if self.rect.left < 0  or self.rect.right > 800:
+            self.xdir =- self.xdir
